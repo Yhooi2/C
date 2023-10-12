22012cd1b078a709b1a927cd242ed6af19b34b74
Yhoo2 <yhooi2011@gmail.com>
2023-10-12 22:35:38 +0300

    sum n recurs

--- test.cpp
+++ test.cpp
@@ -1,35 +1,19 @@
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 
-int iabs(int x) { return (x < 0) ? -x : x; }
+int sumAllNumb(int n, int sum) {
+    if(n < 10) return sum += n;
 
-int eu_mod(int x, int y) {
-    int r;
-    assert(y != 0);
-    r = x % y;
-    if (r < 0)
-        r += iabs(y);
-    return r;
-}
+    return sum + n % 10 + sumAllNumb(n /10 , sum);
+} 
 
-long long gcd(long long x, long long y) {
-    long long q;
-    q = eu_mod(x, y);
-    while(q !=0) {
-        x = y;
-        y = q;
-        q = eu_mod(x, y);
-    } return y;
+int sumAllNumb(int n) {
+    int sum = 0;
+    return sumAllNumb(n, sum);
 }
 
 int main() {
-  long long x = 0, y = 0, g;
-  long long res;
-  
-  res = scanf("%lld %lld", &x, &y);
-  assert(res == 2);
-  g = gcd(x, y);
-  printf("%lld\n", g);
-  return 0;
+
+    int n;
+    std::cin >> n;
+    std::cout << sumAllNumb(n);
 }
