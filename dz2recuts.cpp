3756ea6951d279a3c1e67cf7f985660061d31a4e
Yhoo2 <yhooi2011@gmail.com>
2023-10-13 14:02:15 +0300

    dz check Palindrome

--- dz2recuts.cpp
+++ dz2recuts.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
 
-void printReverse(int n) {
-    if(n < 10) {
-        std::cout << n;
-        return;
-    }
-    std::cout << n%10 <<',';
-    printReverse(n/10);
+bool checkPalindrome(std::string::iterator begin, std::string::iterator end) {
+if(begin + 1 == end -1 || begin + 1 > end - 1) return *begin == *end ? true : false;
+if(*begin != *end) return false; 
+return checkPalindrome(++begin, --end);
+}
 
+bool checkPalindrome(std::string& s) {
+    if(s.empty()) return false;
+    if(s.length() == 1) return false;
+    return checkPalindrome(s.begin(), --s.end());
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    printReverse(n);
+    std::string s;
+    std::cin >> s;
+    std::cout << checkPalindrome(s);
 }
