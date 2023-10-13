a4a169d66bf50b840dd65ff7ba4fc7e9c27880da
Yhoo2 <yhooi2011@gmail.com>
2023-10-13 11:05:45 +0300

    max v recurs

--- dz2recuts.cpp
+++ dz2recuts.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 #include<vector>
 
-int findMax(std::vector<int>::iterator begin, std::vector<int>::iterator end, int sum) {
-    if(begin + 1 == end) return *begin;
-    return sum + *begin + findMax(begin + 1, end, sum);
+int findMax(std::vector<int>::iterator begin, std::vector<int>::iterator end) {
+    if(begin + 2 == end) return *(begin + 2) > *(begin + 1) ? *(begin + 2) : *(begin + 1);
+    int maxRemnant = findMax(++begin, end);
+    return *begin > maxRemnant ? *begin : maxRemnant; 
 }
 int findMax(std::vector<int> arr, int size) {
     if (size == 0) return 0;
     if (size == 1) return arr[0];
-    int sum = 0;
-    return findMax(arr.begin(), arr.end(), sum);
+    return findMax(arr.begin(), arr.end() - 1);
 }
 int main() {
     int size;
