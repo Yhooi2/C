734b88ca5c93b5e687814980e482c12e20592e0e
Yhoo2 <yhooi2011@gmail.com>
2023-10-13 13:13:04 +0300

    revers numb

--- dz2recuts.cpp
+++ dz2recuts.cpp
@@ -1,22 +1,17 @@
 #include<iostream>
-#include<vector>
 
-int findMax(std::vector<int>::iterator begin, std::vector<int>::iterator end) {
-    if(begin + 2 == end) return *(begin + 2) > *(begin + 1) ? *(begin + 2) : *(begin + 1);
-    int maxRemnant = findMax(++begin, end);
-    return *begin > maxRemnant ? *begin : maxRemnant; 
-}
-int findMax(std::vector<int> arr, int size) {
-    if (size == 0) return 0;
-    if (size == 1) return arr[0];
-    return findMax(arr.begin(), arr.end() - 1);
-}
-int main() {
-    int size;
-    std::cin >> size;
-    std::vector<int> arr(size);
-    for(int i = 0; i < size; ++i) {
-        std::cin >> arr[i];
+void printReverse(int n) {
+    if(n < 10) {
+        std::cout << n;
+        return;
     }
-    std::cout << findMax(arr, size);
+    std::cout << n%10 <<',';
+    printReverse(n/10);
+
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    printReverse(n);
 }
