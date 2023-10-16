8c789a51c5fd82d1c871ba91a30d3c3a8987be42
Yhoo2 <yhooi2011@gmail.com>
2023-10-17 00:28:54 +0300

    dz 2

--- dz2recuts.cpp
+++ dz2recuts.cpp
@@ -1,13 +1,20 @@
 #include<iostream>
+#include<vector>
 
-int getDigitsSum(int n) {
-    if(n < 10) return n;
-    int DigitsSum = getDigitsSum(n/10);
-    return n % 10 + DigitsSum;
+int getMax(auto begin, auto end) {
+    if(begin + 1 == end) return *end;
+    int carrNum = getMax(++begin, end);
+    return *begin > carrNum ? *begin : carrNum; 
 }
-
+int getMax(std::vector<int> arr) {
+    return getMax(arr.begin(), arr.end() - 1);
+}
 int main() {
-    int n;
-    std::cin >> n;
-    std::cout << getDigitsSum(n);
+    int size;
+    std::cin >> size;
+    std::vector<int> arr(size);
+    for(int i = 0; i < size; ++i) {
+        std::cin >> arr[i];
+    }
+    std::cout << getMax(arr);
 }
