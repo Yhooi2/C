75577615149e259bee13860b722fad50f609650f
Yhoo2 <yhooi2011@gmail.com>
2023-10-16 23:56:37 +0300

    dz 1

--- dz2recuts.cpp
+++ dz2recuts.cpp
@@ -1,19 +1,13 @@
 #include<iostream>
 
-bool checkPalindrome(std::string::iterator begin, std::string::iterator end) {
-if(begin + 1 == end -1 || begin + 1 > end - 1) return *begin == *end ? true : false;
-if(*begin != *end) return false; 
-return checkPalindrome(++begin, --end);
-}
-
-bool checkPalindrome(std::string& s) {
-    if(s.empty()) return false;
-    if(s.length() == 1) return false;
-    return checkPalindrome(s.begin(), --s.end());
+int getDigitsSum(int n) {
+    if(n < 10) return n;
+    int DigitsSum = getDigitsSum(n/10);
+    return n % 10 + DigitsSum;
 }
 
 int main() {
-    std::string s;
-    std::cin >> s;
-    std::cout << checkPalindrome(s);
+    int n;
+    std::cin >> n;
+    std::cout << getDigitsSum(n);
 }
